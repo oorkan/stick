8b46b1708ab84859f101f4d1f65aca9e571d7bc3
oorkan <oorkan.dev@gmail.com>
2020-07-11 03:42:05 +0400

    cleanup

--- stick.js
+++ stick.js
@@ -7,7 +7,7 @@
     container.classList.add('container', 'full-x', 'full-y', 'fixed');
     moveable.classList.add('moveable', 'absolute');
 
-    container = document.body.appendChild(container);
+    container = dom.body.appendChild(container);
     moveable = container.appendChild(moveable);
 
     const move = (e) => {
