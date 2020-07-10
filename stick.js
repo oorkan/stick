2101bd4b2b84ec96b6fb3446645c0ac0683b22b0
oorkan <oorkan.dev@gmail.com>
2020-07-10 21:21:36 +0400

    add drag

--- stick.js
+++ stick.js
@@ -1,6 +1,7 @@
 (dom => {
     let container = dom.createElement('div'),
-    moveable = dom.createElement('div');
+    moveable = dom.createElement('div'),
+    x0 = 0, y0 = 0;
 
     container.classList.add('container', 'full-x', 'full-y', 'fixed');
     moveable.classList.add('moveable', 'absolute');
@@ -8,9 +9,18 @@
     container = document.body.appendChild(container);
     moveable = container.appendChild(moveable);
 
-    moveable.addEventListener('mousedown', () => {
-        moveable.addEventListener('mousemove', () => {
-            console.log('drag');
-        });        
+    const move = (e) => {
+        moveable.style.left = `${e.pageX - x0}px`;
+        moveable.style.top = `${e.pageY - y0}px`;
+    };
+    
+    moveable.addEventListener('mousedown', (e) => {
+        x0 = e.pageX - e.target.offsetLeft;
+        y0 = e.pageY - e.target.offsetTop;
+        window.addEventListener('mousemove', move );
+    });
+
+    window.addEventListener('mouseup', (e) => {
+        window.removeEventListener('mousemove', move);
     });
 })(document);
